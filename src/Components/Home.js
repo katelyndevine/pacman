0894a393d798d2320b1778d20ca630850fb9ec56
Katelyn Devine <katelyndevine.design@gmail.com>
2021-09-02 09:29:43 -0500

    chore: add technologies used within Home component

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,6 +1,10 @@
 import React from "react";
 import pacmanGIF from "../images/pacman-gif.gif";
 import pacmanLogo from "../images/pacman-logo2.png";
+import js from "../images/JS.png";
+import html from "../images/html.png";
+import css from "../images/css.png";
+import reactLogo from "../images/react.png";
 import { Link } from "react-scroll";
 
 const Home = () => {
@@ -14,8 +18,14 @@ const Home = () => {
           <a href="https://mintbean.io/" target="blank" id="mintbean">
             Mintbean
           </a>{" "}
-          Hackathon event. :D{" "}
+          Hackathon event. Technologies used:{" "}
         </h5>
+        <div className="technologies">
+          <img src={html} alt="HTML5 Logo" className="technology" />
+          <img src={css} alt="CSS3 Logo" className="technology" />
+          <img src={js} alt="JavaScript Logo" className="technology" />
+          <img src={reactLogo} alt="React Logo" className="technology" />
+        </div>
         <div className="homeButtons">
           <Link to="rules" className="homeBtn" spy={true} smooth={true}>
             RULES
